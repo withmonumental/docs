259fc6f1420a86319556f121b08ffc986bfd9d09
Lionel Leiva-Marcon <llm@d-flow.fr>
2023-03-15 19:54:15 +0100

    Test deploy and publish

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,11 +6,10 @@ export default defineUserConfig({
     title : "Monumental Docs",
     description : "Monumental guide for monumental users",
     base: "/docs/",
-
+    dest: "public",
     head: [
         ['link', {rel: 'icon', href: '/logo.png'}]
     ],
-
     locales: {
         "/": {
             lang: "en-US",
