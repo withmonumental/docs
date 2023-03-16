e87cfa7bfa60ce01ccf800c7faec850e05919cf1
withmonumental <lionel@monumental.app>
2023-03-16 17:20:40 +0000

    deploy ac721ec3

--- assets/mermaid-mindmap.esm.min-df0b941d.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app-018f9b2d.js";import"./framework-e555924d.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-268cad43.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
