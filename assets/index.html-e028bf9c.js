d64ceac95c9e129fa34a21745e8d30df29d84c26
withmonumental <lionel@monumental.app>
2023-06-28 10:16:38 +0000

    Upgrade to last vuepress version. Enhancement : include files  (48401409)

--- assets/index.html-e028bf9c.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,Y as t,Z as n,a0 as a,J as c}from"./framework-e555924d.js";const _={};function r(s,l){const e=c("AutoCatalog");return t(),n("div",null,[a(e)])}const d=o(_,[["render",r],["__file","index.html.vue"]]);export{d as default};
